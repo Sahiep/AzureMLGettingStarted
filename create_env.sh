951e23d94428effa9adeca2bce9a2d2ef5013986
Oliver Lohmann <oliver@lohmann.io>
2019-10-28 21:16:29 +0100

    Added AppInsights deployment.

--- create_env.sh
+++ create_env.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-export PREFIX=user03
-export STORAGE_ACCOUNT_NAME=${PREFIX}23
-export ACR_NAME=${PREFIX}23
-export KV_NAME=${PREFIX}23
-export RG_NAME=${PREFIX}_rg
+export PREFIX=contosoml
+export RG_NAME=${PREFIX}_RG
 export LOCATION=northeurope
-export WORKSPACE_NAME=${PREFIX}ml23
+
+export STORAGE_ACCOUNT_NAME=${PREFIX}
+export ACR_NAME=${PREFIX}
+export KV_NAME=${PREFIX}
+export APP_INSIGHTS_NAME=${PREFIX}
+export WORKSPACE_NAME=${PREFIX}
+
+# Add the Azure CLI Extension to work with Application Insights
+az extension add --name application-insights
 
 # Add the Azure CLI Extension to work with ML Workspaces
 az extension add --name azure-cli-ml
@@ -14,7 +19,8 @@ az extension add --name azure-cli-ml
 # Create a Resource Group
 az group create \
     --name ${RG_NAME} \
-	--location ${LOCATION}
+    --location ${LOCATION} \
+    --output none
 
 # Create a Storage Account
 az storage account create \
@@ -27,6 +33,7 @@ az storage account create \
     --https-only "true" \
     --default-action "Allow" \
     --bypass "None" \
+    --kind "StorageV2" \
     --output none
 
 STORAGE_ACCOUNT_ID=$(az storage account list --query "[?name=='${STORAGE_ACCOUNT_NAME}'].{id:id}[0]" | jq -r '.id')
@@ -34,27 +41,38 @@ STORAGE_ACCOUNT_ID=$(az storage account list --query "[?name=='${STORAGE_ACCOUNT
 # Create a Container Registry
 az acr create \
     --name ${ACR_NAME} \
-	--resource-group "${RG_NAME}" \
-	--location ${LOCATION} \
-	--admin-enabled "true" \
-	--sku Standard
+    --resource-group "${RG_NAME}" \
+    --location ${LOCATION} \
+    --admin-enabled "true" \
+    --sku Standard \
+    --output none
+
 
 ACR_ID=$(az acr list --query "[?name=='${ACR_NAME}'].{id:id}[0]" | jq -r '.id')
 
 # Create a KeyVault
 az keyvault create \
     --name ${KV_NAME} \
-	--resource-group "${RG_NAME}" \
-	--location ${LOCATION}
+    --resource-group "${RG_NAME}" \
+    --location ${LOCATION} \
+    --output none
 
 KV_ID=$(az keyvault list --query "[?name=='${KV_NAME}'].{id:id}[0]" | jq -r '.id')
 
+az monitor app-insights component create \
+    --app ${APP_INSIGHTS_NAME} \
+    --location ${LOCATION} \
+    --resource-group ${RG_NAME} \
+    --output none
+
+APP_INSIGHTS_ID=$(az monitor app-insights component show --query "[?applicationId=='${APP_INSIGHTS_NAME}'].{id:id}[0]" | jq -r '.id')
 
 # Finally create the ML Workspace referencing all components...
 az ml workspace create \
     --workspace-name ${WORKSPACE_NAME} \
     --keyvault ${KV_ID} \
-	--resource-group ${RG_NAME} \
-	--location ${LOCATION} \
+    --resource-group ${RG_NAME} \
+    --location ${LOCATION} \
     --container-registry ${ACR_ID} \
-	--storage-account ${STORAGE_ACCOUNT_ID}
+    --application-insights ${APP_INSIGHTS_ID} \
+    --storage-account ${STORAGE_ACCOUNT_ID}
